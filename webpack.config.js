cb5cc762e6b368466c878a0e3bf849f3abe6f9a4
Luke Vandekieft <vandekie@gmail.com>
2018-09-30 19:46:29 -0700

    Add ESLint and typescript.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
- 
+
 module.exports = {
 
   entry: './src/index.js',
@@ -48,7 +48,11 @@ module.exports = {
           'html-loader'
         ]
       },
-
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      },
     ]
   },
 
